b93c9de68a024003b2dc30bf4176109599e92197
daguerrav <da.guerrav@duocuc.cl>
2023-07-08 20:41:45 +0000

    Item 1, 2 y 3 Examen

---
+++ Item1.py
@@ -0,0 +1,4 @@
+print("Examen Transversal Programación y Redes Virtualizadas – DRY7122")
+nombres = ["Sebastián Pereira", "Sebastián Silva", "Sebastián Quinteros", "Juan Pablo Varela", "David Guerra"]
+for nombre in nombres:
+  print(nombre)
